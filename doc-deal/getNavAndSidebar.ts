2ed1b83eddd501b81e0ecf0643d5fc3934b0e5c6
zqylzcwcxy <347891134@qq.com>
2023-02-12 17:53:12 +0800

    :art:improve code structure or code format : 去除多余的包

--- doc-deal/getNavAndSidebar.ts
+++ doc-deal/getNavAndSidebar.ts
@@ -1,29 +1,9 @@
 import fs from 'fs'
 import path from 'path'
-import chokidar from 'chokidar'
 
 // 存放md文件的文件夹名
 const mdDirName = 'md'
 const mdFilePath = path.join(__dirname, '../docs/' + mdDirName)
-
-// 监听目录文件发生变化
-function watchDir() {
-  const watcher = chokidar.watch(mdFilePath, {
-    persistent: true,
-  })
-  watcher.on('add', (path, stats) => {
-    console.log(`File ${path} has been added`)
-  })
-  watcher.on('change', (path, stats) => {
-    console.log(`File ${path} has been changed`)
-  })
-  watcher.on('unlink', (path, stats) => {
-    console.log(`File ${path} has been removed`)
-  })
-  watcher.on('error', error => {
-    console.log(`Watcher error: ${error}`)
-  })
-}
 
 interface sidebar {
   text: string
@@ -50,7 +30,6 @@ export function getSidebar(sidebar = <sidebar[]>[]) {
       })
       // 按照文档前面数字进行排序
       sidebar[index].items = sidebar[index].items.sort((after, before) => {
-        console.log(after)
         return (
           Number(after.text.slice(0, after.text.indexOf('.'))) -
           Number(before.text.slice(0, before.text.indexOf('.')))
@@ -86,7 +65,6 @@ export function getNav(nav = <nav[]>[]) {
     })
     // 按照文档前面数字进行排序
     nav[index].items = nav[index].items.sort((after, before) => {
-      console.log(after)
       return (
         Number(after.text.slice(0, after.text.indexOf('.'))) -
         Number(before.text.slice(0, before.text.indexOf('.')))
